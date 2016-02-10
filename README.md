5881566aa2f165cf9170bfca492da57ea2778158
VonD <paul.vonderscher@gmail.com>
2016-02-10 22:16:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+### Pages
+
+No fuss, opinionated pagination helper.
+
+```es6
+import computePages from 'pages'
+
+computePages({
+	totalSize: 10,
+	currentIndex: 5,
+	bufferSize: 1
+});
+
+/*
+returns :
+    [
+        {page: 1},
+        {isSeparator: true},
+        {page: 4},
+        {page: 5, isCurrent: true},
+        {page: 6},
+        {isSeparator: true},
+        {page: 10}
+    ]
+which could be templated :
+    1 … 4 5 6 … 10
+*/
+```
---
+++ index.js
@@ -0,0 +1,30 @@
+export default function computePages({
+	totalSize = 3,
+	currentIndex = 1,
+	bufferSize = 1
+}) {
+	if (currentIndex < 1 || currentIndex > totalSize) throw('Invalid config');
+	let pages = [{page: currentIndex, isCurrent: true}];
+	for (let page = currentIndex - 1; page >= Math.max(1, currentIndex - bufferSize); page--) {
+		pages.unshift({ page });
+	}
+	for (let page = currentIndex + 1; page <= Math.min(totalSize, currentIndex + bufferSize); page++) {
+		pages.push({ page });
+	}
+	if (pages[0].page > 3) {
+		pages = [{page: 1}, {isSeparator: true}].concat(pages);
+	} else if (pages[0].page === 3) {
+		pages = [{page: 1}, {page: 2}].concat(pages);
+	} else if (pages[0].page === 2) {
+		pages = [{page: 1}].concat(pages);
+	}
+	let lastPage = pages[pages.length - 1].page;
+	if (lastPage > totalSize - 2) {
+		pages = pages.concat([{isSeparator: true}, {page: totalSize}]);
+	} else if (lastPage === totalSize - 2) {
+		pages = pages.concat([{page: totalSize - 1}, {page: totalSize}]);
+	} else if (lastPage === totalSize - 1) {
+		pages = pages.concat([{page: totalSize}]);
+	}
+	return pages;
+}
